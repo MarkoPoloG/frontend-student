aa0cdc9fe0f5e0088ea8b3fb997e2b0b9498a1a2
MarkoPoloG <supersonicbrian12@gmail.com>
2025-03-18 03:22:11 +1100

    s

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,76 +1,110 @@
-import Navbar from "./components/Teacher_Components/Navbar"; // Teacher Navbar (Unused for now)
-import Dashboard from "./views/Teacher_Pages/Dashboard"; // Teacher Dashboard (Unused for now)
-import "@/css/App.css";
+import Announcements from "@/components/Student_components/Announcements";
+import UserDetails from "../../components/Student_components/Announcements";
+import Links from "../../components/Student_components/Announcements";
+import { useNavigate } from "react-router-dom";
+import Navbar from "../../components/Student_components/Announcements";
 
-// Teacher Pages (Kept for Future)
-import UpdateGrades from "./views/Teacher_Pages/UpdateGrades";
-import UpdateResources from "./views/Teacher_Pages/UpdateResources";
-import PostAnnouncements from "./views/Teacher_Pages/PostAnnouncements";
-import TakeAttendance from "./views/Teacher_Pages/TakeAttendance";
-
-// Student Pages (Functional)
-import StudentNavbar from "./components/Student_components/Navbar";
-import StudentDashboard from "./views/Student_Pages/DashboardPage";
-
-// CMS Pages (Admin, Parent, Student Management)
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Admin_SignUp from "./views/CMS_Pages/admin_signup";
-import Admin_LogIn from "./views/CMS_Pages/admin_login";
-import CMS_Dashboard from "./views/CMS_Pages/CMS_dashboard";
-import ParentAccount from "./views/CMS_Pages/Parent_account";
-import ParentCreate from "./views/CMS_Pages/Parent_create.jsx";
-import ParentSelect from "./views/CMS_Pages/Parent_select.jsx";
-import ParentUpdate from "./views/CMS_Pages/Parent_update.jsx";
-import StudentAccount from "./views/CMS_Pages/Student_account.jsx";
-import StudentCreate from "./views/CMS_Pages/Student_create.jsx";
-import StudentSelect from "./views/CMS_Pages/Student_select.jsx";
-import StudentUpdate from "./views/CMS_Pages/Student_update.jsx";
-import Homepage from "./views/CMS_Pages/CMS_homepage";
-import SuccessfulAccount from "./views/CMS_Pages/Student_confirm";
-import CreateWebsitePage from "./views/CMS_Pages/CreateWebsite";
-import WebsiteDesignPage from "./views/CMS_Pages/WebsiteDesign";
-import AccountUpdateConfirmation from "./views/CMS_Pages/Account_update_confirmation";
-import Layout from "./views/CMS_Pages/Layout.jsx";
-
-export default function App() {
+export default function Dashboard() {
+  const navigate = useNavigate();
+  function handleRouting(type) {
+    if (type === "Grades") {
+      navigate("/grades");
+    } else if (type === "Resources") {
+      navigate("/resources");
+    } else if (type === "Announcements") {
+      navigate("/announcements");
+    } else if (type === "Attendance") {
+      navigate("/attendance");
+    }
+  }
   return (
-    <Router>
-      <div className="App">
-        {/* Use Student Navbar for now */}
-        <StudentNavbar />
-
-        <Routes>
-          {/* Student Dashboard is active instead of Teacher Dashboard */}
-          <Route path="/" element={<StudentDashboard />} />
-
-          {/* Teacher Routes (Kept for Future Use) */}
-          <Route path="/teacher/dashboard" element={<Dashboard />} />
-          <Route path="/teacher/grades" element={<UpdateGrades />} />
-          <Route path="/teacher/resources" element={<UpdateResources />} />
-          <Route path="/teacher/announcements" element={<PostAnnouncements />} />
-          <Route path="/teacher/attendance" element={<TakeAttendance />} />
-
-          {/* CMS/Admin Routes */}
-          <Route path="/cms" element={<Layout />}>
-            <Route index element={<Homepage />} />
-            <Route path="signup" element={<Admin_SignUp />} />
-            <Route path="login" element={<Admin_LogIn />} />
-            <Route path="parentcreate" element={<ParentCreate />} />
-            <Route path="parentselect" element={<ParentSelect />} />
-            <Route path="studentaccount" element={<StudentAccount />} />
-            <Route path="studentcreate" element={<StudentCreate />} />
-            <Route path="studentselect" element={<StudentSelect />} />
-            <Route path="edit-student/:studentId" element={<StudentUpdate />} />
-            <Route path="edit-parent/:parentId" element={<ParentUpdate />} />
-            <Route path="AccountSuccessful" element={<SuccessfulAccount />} />
-            <Route path="AccountUpdateSuccessful" element={<AccountUpdateConfirmation />} />
-            <Route path="websitecreate" element={<CreateWebsitePage />} />
-            <Route path="dashboard" element={<CMS_Dashboard />} />
-            <Route path="parentmanagement" element={<ParentAccount />} />
-            <Route path="websitedesign" element={<WebsiteDesignPage />} />
-          </Route>
-        </Routes>
+    <div id="container" style={{ padding: "20px" }}>
+      <div>
+        <Navbar />
       </div>
-    </Router>
+      <div className="mx-auto bg-light rounded-bottom">
+        <UserDetails />
+        <Announcements />
+        <div className="container w-100 mx-auto" style={{ marginTop: "100px" }}>
+          <div className="row ">
+            <div className="col">
+              <Links
+                type="Update Grades"
+                onSubmit={() => handleRouting("Grades")}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="50"
+                  height="50"
+                  fill="currentColor"
+                  className="bi bi-journal-bookmark mx-auto d-block my-5"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M6 8V1h1v6.117L8.743 6.07a.5.5 0 0 1 .514 0L11 7.117V1h1v7a.5.5 0 0 1-.757.429L9 7.083 6.757 8.43A.5.5 0 0 1 6 8"
+                  />
+                  <path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2" />
+                  <path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1z" />
+                </svg>
+              </Links>
+            </div>
+            <div className="col">
+              <Links
+                type="Update Resources"
+                onSubmit={() => handleRouting("Resources")}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="50"
+                  height="50"
+                  fill="currentColor"
+                  className="bi bi-folder mx-auto d-block my-5"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M.54 3.87.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.826a2 2 0 0 1-1.991-1.819l-.637-7a2 2 0 0 1 .342-1.31zM2.19 4a1 1 0 0 0-.996 1.09l.637 7a1 1 0 0 0 .995.91h10.348a1 1 0 0 0 .995-.91l.637-7A1 1 0 0 0 13.81 4zm4.69-1.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139q.323-.119.684-.12h5.396z" />
+                </svg>
+              </Links>
+            </div>
+            <div className="col">
+              <Links
+                type="Post Announcements"
+                onSubmit={() => handleRouting("Announcements")}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="50"
+                  height="50"
+                  fill="currentColor"
+                  className="bi bi-bell mx-auto d-block my-5"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2M8 1.918l-.797.161A4 4 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4 4 0 0 0-3.203-3.92zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5 5 0 0 1 13 6c0 .88.32 4.2 1.22 6" />
+                </svg>
+              </Links>
+            </div>
+            <div className="col">
+              <Links
+                type="Take Attendance"
+                onSubmit={() => handleRouting("Attendance")}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="50"
+                  height="50"
+                  fill="currentColor"
+                  class="bi bi-calendar2-date mx-auto d-block my-5"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M6.445 12.688V7.354h-.633A13 13 0 0 0 4.5 8.16v.695c.375-.257.969-.62 1.258-.777h.012v4.61zm1.188-1.305c.047.64.594 1.406 1.703 1.406 1.258 0 2-1.066 2-2.871 0-1.934-.781-2.668-1.953-2.668-.926 0-1.797.672-1.797 1.809 0 1.16.824 1.77 1.676 1.77.746 0 1.23-.376 1.383-.79h.027c-.004 1.316-.461 2.164-1.305 2.164-.664 0-1.008-.45-1.05-.82zm2.953-2.317c0 .696-.559 1.18-1.184 1.18-.601 0-1.144-.383-1.144-1.2 0-.823.582-1.21 1.168-1.21.633 0 1.16.398 1.16 1.23" />
+                  <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M2 2a1 1 0 0 0-1 1v11a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1z" />
+                  <path d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5z" />
+                </svg>
+              </Links>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
